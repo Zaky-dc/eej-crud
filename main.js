398b676cf2a26f89a585f122f842bd0e6be29595
ZakirMagide <62655140+Zaky-dc@users.noreply.github.com>
2025-01-09 22:13:03 +0200

    main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ app.listen(PORT,()=>{
     console.log(`server started at http://localhost:${PORT}`)
 })
 
-cron.schedule('02 22 * * *', async () => {
+cron.schedule('13 22 * * *', async () => {
     try {
         const hoje = new Date().toISOString().split('T')[0]; // Data atual no formato AAAA-MM-DD
 
